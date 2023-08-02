7eb1a98b92a5c685f20f04a7250e79974931ecdb
L_Finistao <61809466+L-Finistao@users.noreply.github.com>
2023-08-02 10:30:49 -0300

    Completo

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,18 +28,209 @@
         /// </summary>
         private void InitializeComponent()
         {
+            Bt09 = new Button();
+            Bt01 = new Button();
+            Bt04 = new Button();
+            Bt07 = new Button();
+            Bt05 = new Button();
+            Bt02 = new Button();
+            Bt03 = new Button();
+            Bt08 = new Button();
+            Bt06 = new Button();
+            pictureBox1 = new PictureBox();
+            RestartButton = new Button();
+            button10 = new Button();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
+            // 
+            // Bt09
+            // 
+            Bt09.BackColor = SystemColors.Window;
+            Bt09.Location = new Point(238, 249);
+            Bt09.Name = "Bt09";
+            Bt09.Size = new Size(67, 59);
+            Bt09.TabIndex = 0;
+            Bt09.UseVisualStyleBackColor = false;
+            Bt09.Click += button_Click;
+            Bt09.MouseLeave += verificar;
+            // 
+            // Bt01
+            // 
+            Bt01.BackColor = SystemColors.Window;
+            Bt01.Location = new Point(92, 119);
+            Bt01.Name = "Bt01";
+            Bt01.Size = new Size(67, 59);
+            Bt01.TabIndex = 1;
+            Bt01.UseVisualStyleBackColor = false;
+            Bt01.Click += button_Click;
+            Bt01.MouseEnter += verificar;
+            // 
+            // Bt04
+            // 
+            Bt04.BackColor = SystemColors.Window;
+            Bt04.Location = new Point(92, 184);
+            Bt04.Name = "Bt04";
+            Bt04.Size = new Size(67, 59);
+            Bt04.TabIndex = 2;
+            Bt04.UseVisualStyleBackColor = false;
+            Bt04.Click += button_Click;
+            Bt04.MouseLeave += verificar;
+            // 
+            // Bt07
+            // 
+            Bt07.BackColor = SystemColors.Window;
+            Bt07.Location = new Point(92, 249);
+            Bt07.Name = "Bt07";
+            Bt07.Size = new Size(67, 59);
+            Bt07.TabIndex = 3;
+            Bt07.UseVisualStyleBackColor = false;
+            Bt07.Click += button_Click;
+            Bt07.MouseLeave += verificar;
+            // 
+            // Bt05
+            // 
+            Bt05.BackColor = SystemColors.Window;
+            Bt05.Location = new Point(165, 184);
+            Bt05.Name = "Bt05";
+            Bt05.Size = new Size(67, 59);
+            Bt05.TabIndex = 4;
+            Bt05.UseVisualStyleBackColor = false;
+            Bt05.Click += button_Click;
+            Bt05.MouseLeave += verificar;
+            // 
+            // Bt02
+            // 
+            Bt02.BackColor = SystemColors.Window;
+            Bt02.Location = new Point(165, 119);
+            Bt02.Name = "Bt02";
+            Bt02.Size = new Size(67, 59);
+            Bt02.TabIndex = 5;
+            Bt02.UseVisualStyleBackColor = false;
+            Bt02.Click += button_Click;
+            Bt02.MouseLeave += verificar;
+            // 
+            // Bt03
+            // 
+            Bt03.BackColor = SystemColors.Window;
+            Bt03.Location = new Point(238, 120);
+            Bt03.Name = "Bt03";
+            Bt03.Size = new Size(67, 59);
+            Bt03.TabIndex = 6;
+            Bt03.UseVisualStyleBackColor = false;
+            Bt03.Click += button_Click;
+            Bt03.MouseLeave += verificar;
+            // 
+            // Bt08
+            // 
+            Bt08.BackColor = SystemColors.Window;
+            Bt08.Location = new Point(165, 249);
+            Bt08.Name = "Bt08";
+            Bt08.Size = new Size(67, 59);
+            Bt08.TabIndex = 7;
+            Bt08.UseVisualStyleBackColor = false;
+            Bt08.Click += button_Click;
+            Bt08.MouseLeave += verificar;
+            // 
+            // Bt06
+            // 
+            Bt06.BackColor = SystemColors.Window;
+            Bt06.Location = new Point(238, 184);
+            Bt06.Name = "Bt06";
+            Bt06.Size = new Size(67, 59);
+            Bt06.TabIndex = 8;
+            Bt06.UseVisualStyleBackColor = false;
+            Bt06.Click += button_Click;
+            Bt06.MouseLeave += verificar;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = SystemColors.ActiveCaptionText;
+            pictureBox1.Location = new Point(92, 119);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(213, 189);
+            pictureBox1.TabIndex = 9;
+            pictureBox1.TabStop = false;
+            // 
+            // RestartButton
+            // 
+            RestartButton.BackColor = SystemColors.ActiveCaption;
+            RestartButton.ForeColor = Color.MidnightBlue;
+            RestartButton.Location = new Point(12, 347);
+            RestartButton.Name = "RestartButton";
+            RestartButton.Size = new Size(124, 41);
+            RestartButton.TabIndex = 10;
+            RestartButton.Text = "Reiniciar";
+            RestartButton.UseVisualStyleBackColor = false;
+            RestartButton.Click += Restart_Game;
+            // 
+            // button10
+            // 
+            button10.BackColor = SystemColors.ActiveCaption;
+            button10.ForeColor = Color.MidnightBlue;
+            button10.Location = new Point(264, 347);
+            button10.Name = "button10";
+            button10.Size = new Size(124, 41);
+            button10.TabIndex = 12;
+            button10.Text = "SAIR";
+            button10.UseVisualStyleBackColor = false;
+            button10.Click += button10_Click;
+            // 
+            // label1
+            // 
+            label1.BackColor = SystemColors.ButtonFace;
+            label1.BorderStyle = BorderStyle.FixedSingle;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.Red;
+            label1.Location = new Point(98, 69);
+            label1.Name = "label1";
+            label1.Size = new Size(207, 48);
+            label1.TabIndex = 13;
+            label1.Text = "teste";
+            label1.TextAlign = ContentAlignment.TopCenter;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(547, 450);
+            BackgroundImage = Properties.Resources.istockphoto_989794366_170667a;
+            BackgroundImageLayout = ImageLayout.Center;
+            ClientSize = new Size(400, 400);
+            Controls.Add(label1);
+            Controls.Add(button10);
+            Controls.Add(RestartButton);
+            Controls.Add(Bt06);
+            Controls.Add(Bt08);
+            Controls.Add(Bt03);
+            Controls.Add(Bt02);
+            Controls.Add(Bt05);
+            Controls.Add(Bt07);
+            Controls.Add(Bt04);
+            Controls.Add(Bt01);
+            Controls.Add(Bt09);
+            Controls.Add(pictureBox1);
+            ForeColor = Color.Coral;
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Button Bt09;
+        private Button Bt01;
+        private Button Bt04;
+        private Button Bt07;
+        private Button Bt05;
+        private Button Bt02;
+        private Button Bt03;
+        private Button Bt08;
+        private Button Bt06;
+        private PictureBox pictureBox1;
+        private Button RestartButton;
+        private Button button10;
+        private Label label1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,9 +2,107 @@ namespace Jogo_Da_Velha_LucasF
 {
     public partial class Form1 : Form
     {
+        //Variaveis de Controlhe
+        String JogadorVez = "O";
+        bool Vitoria = false;
+        String Ganhador = "";
+
         public Form1()
         {
             InitializeComponent();
+            label1.Text = "";
+            label1.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            if (!Vitoria)
+            {
+                Button button = (Button)sender;
+                if (button.Text == "")
+                    button.Text = JogadorDaVez();
+            }
+        }
+
+        private string JogadorDaVez()
+        {
+            if (JogadorVez == "X")
+            {
+                JogadorVez = "O";
+                return "O";
+            }
+            JogadorVez = "X";
+            return "X";
+
+        }
+
+        private void Restart_Game(object sender, EventArgs e)
+        {
+            Restart_Celuna(Bt01);
+            Restart_Celuna(Bt02);
+            Restart_Celuna(Bt03);
+            Restart_Celuna(Bt04);
+            Restart_Celuna(Bt05);
+            Restart_Celuna(Bt06);
+            Restart_Celuna(Bt07);
+            Restart_Celuna(Bt08);
+            Restart_Celuna(Bt09);
+            Vitoria = false;
+            label1.Text = "";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+        private void Restart_Celuna(Button btn00)
+        {
+            btn00.Text = "";
+
+        }
+
+        private bool VerificarCasos(Button btn00, Button btn01, Button btn02)
+        {
+            if (btn00.Text == btn01.Text && btn01.Text == btn02.Text)
+            {
+                if (btn01.Text != "")
+                {
+                    Vitoria = true;
+                    label1.Visible = true;
+                    Ganhador = "Ganhador - |" + btn01.Text + "|";
+                    label1.Text = Ganhador;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void verificar(object sender, EventArgs e)
+        {
+            //Caso 01 - Linhas
+            VerificarCasos(Bt01, Bt02, Bt03);
+            VerificarCasos(Bt04, Bt05, Bt06);
+            VerificarCasos(Bt07, Bt08, Bt09);
+
+            //Caso 02 - Colunas
+            VerificarCasos(Bt01, Bt04, Bt07);
+            VerificarCasos(Bt02, Bt05, Bt08);
+            VerificarCasos(Bt03, Bt06, Bt09);
+
+            //Caso 03 - Diagonais
+            VerificarCasos(Bt01, Bt05, Bt09);
+            VerificarCasos(Bt03, Bt05, Bt07);
+
+
+
+
+
         }
     }
 }
